2376e7d916133bc5f481263baa3cbd8a48314ee2
EmpyreanArcadius <immaculateempyrean@gmail.com>
2020-04-30 19:08:26 +0530

    Refactored Slightly From Previous Commit And Am Now Actually Using Dll Main Function Now.

--- Workspace/Game/src/Main.cpp
+++ Workspace/Game/src/Main.cpp
@@ -2,16 +2,28 @@
 
 BOOL WINAPI DllMain(_In_ HINSTANCE hinstDLL,_In_ DWORD fdwReason,_In_ LPVOID lpvReserved)
 {
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		//The DLL Is Clearly Attaching We May Use This Chance Initialize Stuff..
+		std::vector<std::pair<std::string, CreateLogFile>> Configuration;
+		Configuration.emplace_back(std::make_pair<std::string, CreateLogFile>("Game", CreateLogFile::CreateLogFile));
+		Log::Init(Configuration);
+
+		Log_Trace("Game", "Game DLL Attach Invoked Simply Tracing It.. :>");
+	}
+	
 	return TRUE;
 }
+
+void DLL __cdecl GetAppFromGame()
+{
+
+}
+
+
 void DLL __cdecl TestFunc()
 {
-	std::vector<std::pair<std::string, CreateLogFile>> Configuration;
-	Configuration.emplace_back(std::make_pair<std::string, CreateLogFile>("Lother", CreateLogFile::CreateLogFile));
-	Log::Init(Configuration);
-
-	//Log::GetDefaultSTDOUTLogger()->trace("This IS Sigmar");
-	STDOUTDefaultLog_Error("I Can Print Here From The DLL");
+	
 	FileDefaultLog_Error("Han");
 
 	std::cout << "Hello World" << std::endl;
